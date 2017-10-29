4c5eb464bab5f7a2e2bc436a35c51d78ac14f9ea
Yury Matuhin <ym@ymatuhin.ru>
2017-10-29 23:09:20 +0300

    Update filling script

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,107 @@
-const CANVAS_WIDTH = 500;
-const CANVAS_HEIGHT = 500;
+import { rgbToHsl, hslToRgb } from './colorConvert';
 
-const img: any = document.getElementById('ouline');
-const canvas: any = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
+const CANVAS_WIDTH = document.documentElement.clientWidth;
+const CANVAS_HEIGHT = document.documentElement.clientHeight;
 
-const imgRatio = img.naturalWidth / img.naturalHeight; // 0.83...
+function init() {
+  const { canvas, ctx } = initCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
+  const img: any = document.getElementById('ouline');
 
-canvas.width = CANVAS_WIDTH;
-canvas.height = CANVAS_HEIGHT;
-
-ctx.drawImage(img, 0, 0, 500, 500);
-
-canvas.addEventListener('click', ({ layerX, layerY }: any) => {
-  fillColor({ x: layerX, y: layerY });
-});
-
-const filled = {};
-function fillColor({ x, y }: any) {
-  if (filled[`${x}:${y}`]) return;
-
-  const color = [255, 0, 0, 0];
-  const imageData = ctx.getImageData(x, y, 1, 1);
-  const isWhite =
-    imageData.data[0] > 100 &&
-    imageData.data[1] > 100 &&
-    imageData.data[2] > 100;
-
-  if (!isWhite) return;
-
-  imageData.data[1] = 0;
-  imageData.data[2] = 0;
-  ctx.putImageData(imageData, x, y);
-  filled[`${x}:${y}`] = true;
-
-  window.requestAnimationFrame(() => {
-    fillColor({ x: x - 1, y });
-    fillColor({ x: x + 1, y });
-    fillColor({ x, y: y + 1 });
-    fillColor({ x, y: y - 1 });
+  drawImageScaled(img, ctx);
+  canvas.addEventListener('click', ({ layerX, layerY }: any) => {
+    const color = [
+      getRandomInt(101, 255),
+      getRandomInt(101, 255),
+      getRandomInt(101, 255),
+    ];
+    fillColor(layerX, layerY, ctx, color);
   });
+}
+
+function initCanvas(width: number, height: number) {
+  const canvas: any = document.getElementById('canvas');
+  const ctx = canvas.getContext('2d');
+
+  canvas.width = width;
+  canvas.height = height;
+  return { canvas, ctx };
+}
+
+function drawImageScaled(img: any, ctx: any) {
+  var canvas = ctx.canvas;
+  var hRatio = canvas.width / img.width;
+  var vRatio = canvas.height / img.height;
+  var ratio = Math.min(hRatio, vRatio);
+  var centerShift_x = (canvas.width - img.width * ratio) / 2;
+  var centerShift_y = (canvas.height - img.height * ratio) / 2;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.drawImage(
+    img,
+    0,
+    0,
+    img.width,
+    img.height,
+    centerShift_x,
+    centerShift_y,
+    img.width * ratio,
+    img.height * ratio,
+  );
+}
+
+function fillColor(x: number, y: number, ctx: any, color: any) {
+  const hslFillColor = rgbToHsl.apply(null, color);
+  const filled = {};
+  const imageData = ctx.getImageData(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  const width = imageData.width;
+  const height = imageData.height;
+  const stack = [[x, y]];
+  const black = 100;
+
+  const redIndex = y * (width * 4) + x * 4;
+  const pointHsl = rgbToHsl(
+    imageData.data[redIndex],
+    imageData.data[redIndex + 1],
+    imageData.data[redIndex + 2],
+  );
+  console.info(`# pointHsl`, pointHsl);
+
+  while (stack.length > 0) {
+    const [localX, localY] = stack.pop();
+    if (filled[`${localX}:${localY}`]) continue;
+
+    const redIndex = localY * (width * 4) + localX * 4;
+    const isTransparent = imageData.data[redIndex + 3] < 20;
+    const pointHsl = rgbToHsl(
+      imageData.data[redIndex],
+      imageData.data[redIndex + 1],
+      imageData.data[redIndex + 2],
+    );
+    const isBlackPoint = pointHsl[2] < 0.4;
+
+    if (isBlackPoint || isTransparent) continue;
+
+    const newColor = hslToRgb(
+      hslFillColor[0],
+      hslFillColor[1],
+      Math.min(hslFillColor[2], pointHsl[2]),
+    );
+    imageData.data[redIndex] = newColor[0];
+    imageData.data[redIndex + 1] = newColor[1];
+    imageData.data[redIndex + 2] = newColor[2];
+    filled[`${localX}:${localY}`] = true;
+
+    // Ставим соседей в стек на проверку
+    stack.push([localX - 1, localY]);
+    stack.push([localX + 1, localY]);
+    stack.push([localX, localY - 1]);
+    stack.push([localX, localY + 1]);
+  }
+
+  ctx.putImageData(imageData, 0, 0);
+}
+
+init();
+
+function getRandomInt(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,78 +1,78 @@
-const webpack = require("webpack");
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const DashboardPlugin = require("webpack-dashboard/plugin");
-const nodeEnv = process.env.NODE_ENV || "development";
-const isProd = nodeEnv === "production";
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const DashboardPlugin = require('webpack-dashboard/plugin');
+const nodeEnv = process.env.NODE_ENV || 'development';
+const isProd = nodeEnv === 'production';
 
 var config = {
-  devtool: isProd ? "hidden-source-map" : "source-map",
-  context: path.resolve("./src"),
+  devtool: isProd ? 'hidden-source-map' : 'source-map',
+  context: path.resolve('./src'),
   entry: {
-    app: "./index.ts",
-    vendor: "./vendor.ts"
+    app: './index.ts',
+    vendor: './vendor.ts',
   },
   output: {
-    path: path.resolve("./dist"),
-    filename: "[name].bundle.js",
-    sourceMapFilename: "[name].bundle.map",
-    devtoolModuleFilenameTemplate: function (info) {
-        return "file:///" + info.absoluteResourcePath;
-    }
+    path: path.resolve('./dist'),
+    filename: '[name].bundle.js',
+    sourceMapFilename: '[name].bundle.map',
+    devtoolModuleFilenameTemplate: function(info) {
+      return 'file:///' + info.absoluteResourcePath;
+    },
   },
   module: {
     rules: [
       {
-        enforce: "pre",
+        enforce: 'pre',
         test: /\.ts?$/,
-        exclude: ["node_modules"],
-        use: ["awesome-typescript-loader", "source-map-loader"]
+        exclude: ['node_modules'],
+        use: ['awesome-typescript-loader', 'source-map-loader'],
       },
-      { test: /\.html$/, loader: "html-loader" },
-      { test: /\.css$/, loaders: ["style-loader", "css-loader"] }
-    ]
+      { test: /\.html$/, loader: 'html-loader' },
+      { test: /\.css$/, loaders: ['style-loader', 'css-loader'] },
+    ],
   },
   resolve: {
-    extensions: [".ts", ".js"]
+    extensions: ['.ts', '.js'],
   },
   plugins: [
     new webpack.DefinePlugin({
-      "process.env": {
+      'process.env': {
         // eslint-disable-line quote-props
-        NODE_ENV: JSON.stringify(nodeEnv)
-      }
+        NODE_ENV: JSON.stringify(nodeEnv),
+      },
     }),
     new HtmlWebpackPlugin({
-      title: "Typescript Webpack Starter",
-      template: "!!ejs-loader!src/index.html"
+      title: 'Раскрасочки',
+      template: '!!ejs-loader!src/index.html',
     }),
     new webpack.optimize.CommonsChunkPlugin({
-      name: "vendor",
+      name: 'vendor',
       minChunks: Infinity,
-      filename: "vendor.bundle.js"
+      filename: 'vendor.bundle.js',
     }),
     new webpack.optimize.UglifyJsPlugin({
       compress: { warnings: false },
       output: { comments: false },
-      sourceMap: true
+      sourceMap: true,
     }),
     new DashboardPlugin(),
     new webpack.LoaderOptionsPlugin({
       options: {
         tslint: {
           emitErrors: true,
-          failOnHint: true
-        }
-      }
-    })
+          failOnHint: true,
+        },
+      },
+    }),
   ],
   devServer: {
-    contentBase: path.join(__dirname, "dist/"),
+    contentBase: path.join(__dirname, 'dist/'),
     compress: true,
     port: 3000,
     hot: true,
-    inline: true
-  }
+    inline: true,
+  },
 };
 
 module.exports = config;
